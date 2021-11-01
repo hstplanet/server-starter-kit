ebaa70a9c4cc8564fc281b0f0c35311328397667
umutaltin <umut.altin@live.com>
2021-11-01 22:06:30 +0300

    Umut

--- utils/index.js
+++ utils/index.js
@@ -168,7 +168,7 @@ module.exports.complete = function (data, { chalk }) {
   
   sortDependencies(data, green);
 
-  const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+  const cwd = path.join(".." , process.cwd(), data.inPlace ? '' : data.destDirName)
 
   if (data.autoInstall) {
     installDependencies(cwd, data.autoInstall, green)
